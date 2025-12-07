78b8c10d58a16f08c999cdb0057baa88620f52b7
Cassandra Arcilla <arcillacassandra009@gmail.com>
2025-12-07 21:19:51 +0800

    second commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { EmployeeDirectory } from './employee-directory/employee-directory';
+import { About } from './about/about';
+import { ProductsServices } from './products-services/products-services';
+import { Pagenotfound } from './pagenotfound/pagenotfound';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', component: Home},
+    {path:'employee-directory', component: EmployeeDirectory},
+    {path:'about', component: About},
+    {path:'products-services', component: ProductsServices},
+    {path:'**', component: Pagenotfound}
+];
