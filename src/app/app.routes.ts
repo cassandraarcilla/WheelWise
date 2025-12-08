e4d2fd0a67fd1525ac6bdfa6e326255935cad26c
Cassandra Arcilla <arcillacassandra009@gmail.com>
2025-12-08 17:37:03 +0800

    addional page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Home } from './home/home';
 import { EmployeeDirectory } from './employee-directory/employee-directory';
 import { About } from './about/about';
 import { ProductsServices } from './products-services/products-services';
+import { BookNow } from './book-now/book-now';
 import { Pagenotfound } from './pagenotfound/pagenotfound';
 
 export const routes: Routes = [
@@ -10,5 +11,7 @@ export const routes: Routes = [
     {path:'employee-directory', component: EmployeeDirectory},
     {path:'about', component: About},
     {path:'products-services', component: ProductsServices},
+    {path:'book-now', component: BookNow},
     {path:'**', component: Pagenotfound}
+    
 ];
